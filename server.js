92602113c8ebf687e6159ccec8957be6ecc189b9
THAYSSA <cg3021157@LAB111M10.IFSP>
2023-03-21 22:16:43 -0300

    a1

--- server.js
+++ server.js
@@ -15,3 +15,15 @@ mongoose.Promise = global.Promise
 mongoose.connection.on('error', (erro) => {
     console.log('Erro: ', error.message)
 });
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
+server.use(fileupload());
+server.use(express.static(__dirname + '/public'));
+
+server.use('/', apiRoutes)
+server.use(process.env.PORT, () => {
+    console.log(`Rodando no endereço: ${process.env.BASEURL}`);
+});
